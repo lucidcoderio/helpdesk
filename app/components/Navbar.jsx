112e92f3a1aab82b9f10e696e21a9d0335f233d8
lucidcoderio <147671842+lucidcoderio@users.noreply.github.com>
2023-11-07 15:18:42 -0700

    Styles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,17 @@
-'use client'
+import Image from 'next/image'
 import Link from 'next/link'
+import Logo from './logo.png'
 
 export default function Navbar() {
     return (
         <nav>
+            <Image 
+                src={Logo}
+                alt="Helpdesk logo"
+                width={70}
+                quality={100}
+                placeholder='blur'
+            />
             <h1>Help Desk</h1>
             <Link href="/">Dashboard</Link>
             <Link href="/tickets">Tickets</Link>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
+import { Rubik  } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={rubik.className}>
         <Navbar/>
         {children}
       </body>
